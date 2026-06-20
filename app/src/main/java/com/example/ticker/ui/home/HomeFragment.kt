99e0c7dd1004f20275877456e6afe4e88e0c58d5
Rajan Bhusal <rajanbhusal@Rajans-MacBook-Pro-2.local>
2026-06-20 13:53:57 +1000

    created onView in HomeFragment

--- app/src/main/java/com/example/ticker/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/ticker/ui/home/HomeFragment.kt
@@ -1,15 +1,28 @@
 package com.example.ticker.ui.home
 
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ticker.databinding.FragmentHomeBinding
 import com.example.ticker.viewmodel.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment: Fragment() {
     private val viewModel: HomeViewModel by viewModels()
-    private var _binding: FragmentHomeBinding? = null //storage box that can be empty or full depending on whether view exist
+    //FragmentHomeBinding -generates class for fragment_home.xml
+    private var _binding: FragmentHomeBinding? = null //Fragment can exist but View can be destroyed,so its prevent memory leaks
      private val binding get() = _binding!!//Whenever I use binding, return _binding, but force it to be non-null
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?){
+        super.onViewCreated(view, savedInstanceState)
+        //binding.searchButton because of FragmentHomeBinding else view.findViewBy
+        binding.searchButton.setOnClickListener{
+            val symbol = binding.searchEditText.text.toString().uppercase()
+            viewModel.loadStock(symbol)
+        }
+
+    }
 }
