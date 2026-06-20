bda0a24ea0cb60d1a2bf3f0991aeb2ec977cbe54
Ramanson Thapa <83455438+RAMAN817@users.noreply.github.com>
2026-06-20 12:14:06 +1000

    completed HomeUIState

--- app/src/main/java/com/example/ticker/viewmodel/HomeViewModel.kt
+++ app/src/main/java/com/example/ticker/viewmodel/HomeViewModel.kt
@@ -1,5 +1,7 @@
 package com.example.ticker.viewmodel
 
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.ticker.data.model.Stock
 import com.example.ticker.data.repository.NewsRepository
 import com.example.ticker.data.repository.StockRepository
@@ -9,34 +11,63 @@ import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
 class HomeViewModel @Inject constructor(
-    private  val stockRepository: StockRepository,
-    private  val newsRepository: NewsRepository)
-    {
-private val _stockState = MutableStateFlow<StockUiState>(StockUiState.Idle)
-val stockState: StateFlow<StockUiState> = _stockState.asStateFlow()
-private val _newsState = MutableStateFlow<NewsUiState>(NewsUiState.Loading)
-val newsState: StateFlow<NewsUiState> = _newsState.asStateFlow()
+    private val stockRepository: StockRepository,
+    private val newsRepository: NewsRepository
+) : ViewModel() {
+    private val _stockState =
+        MutableStateFlow<StockUiState>(StockUiState.Idle)//gets the  Idle when started
+    val stockState: StateFlow<StockUiState> = _stockState.asStateFlow()
+    private val _newsState = MutableStateFlow<NewsUiState>(NewsUiState.Loading)
+    val newsState: StateFlow<NewsUiState> = _newsState.asStateFlow()
 
 
-//because we want to load news once page is open
-init {
-    loadNews()
-}
-        fun loadStock(symbol:String) {
+    //because we want to load news once page is open
+    init {
+        loadNews()
+    }
+
+    fun loadStock(symbol: String) {
+        viewModelScope.launch {
+            _stockState.value = StockUiState.Loading
+            val result = stockRepository.getStock(symbol)
+
+            result.onSuccess { //if  successfully got  symbol
+                    stock ->
+                _stockState.value = StockUiState.Success(stock)
+
+            }.onFailure { exception ->  //else
+                _stockState.value = StockUiState.Error(
+                    exception.message ?: "unknown error"
+                )
+            }
+        }
 
 
+    }
 
+    fun loadNews(category: String = "general") {
+        viewModelScope.launch {
 
+            _newsState.value = NewsUiState.Loading
+            val result = newsRepository.getNews(category)
+            result.onSuccess { //On success, the lambda hands the value
+                    news ->
+                _newsState.value = NewsUiState.Success(news)
+
+            }.onFailure { exception ->  //else
+                _newsState.value = NewsUiState.Error(
+                    exception.message ?: "unknown error"
+                )
+
+            }
 
         }
-        fun loadNews() {
-
-        }
 
 
-
+    }
 }
