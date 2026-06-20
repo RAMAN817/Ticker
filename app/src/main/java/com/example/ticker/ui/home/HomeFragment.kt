9f399fc21b310f5a6332f7bd5baa5becb31e38b3
Ramanson Thapa <83455438+RAMAN817@users.noreply.github.com>
2026-06-20 12:50:23 +1000

    started Fragment

--- app/src/main/java/com/example/ticker/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/ticker/ui/home/HomeFragment.kt
@@ -1,8 +1,15 @@
 package com.example.ticker.ui.home
 
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.ticker.viewmodel.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeFragment: Fragment() {
+    private val viewModel: HomeViewModel by viewModels()
+    private var _binding: FragmentHomeBinding? = null //storage box that can be empty or full depending on whether view exist
+     private val binding get() = _binding!!//Whenever I use binding, return _binding, but force it to be non-null
+
 }
