e4bf1e10508d02e7a246485286c41d6f0ee2b201
Rajan Bhusal <rajanbhusal@Rajans-MBP-2.modem>
2026-06-21 17:38:20 +1000

    created onViewCreated for HomeVeiwModel

--- app/src/main/java/com/example/ticker/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/ticker/ui/home/HomeFragment.kt
@@ -4,10 +4,14 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.ticker.databinding.FragmentHomeBinding
 import com.example.ticker.viewmodel.HomeViewModel
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HomeFragment: Fragment() {
@@ -23,6 +27,17 @@ class HomeFragment: Fragment() {
             val symbol = binding.searchEditText.text.toString().uppercase()
             viewModel.loadStock(symbol)
         }
+        //Starts a coroutine tied to the Fragment’s VIEW lifecycle
+        viewLifecycleOwner.lifecycleScope.launch {
+            viewLifecycleOwner.repeatOnLifecycle(Lifecycle.State.STARTED){ //Turn the listener ON when screen is visible, OFF when hidden
+                //every time state changes (loading, success, error)
+                //react to every new state emitted while listener is ON
+                viewModel.uiState.collect { state ->
+                    render(state)
+                }
+            }
+        }
+
 
     }
 }
