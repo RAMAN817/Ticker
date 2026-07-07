43b94bb7368135073368e9bc6d9ef4b4c1653905
Ramanson Thapa <83455438+RAMAN817@users.noreply.github.com>
2026-07-08 00:34:49 +1000

    First run

--- app/src/main/java/com/example/ticker/MainActivity.kt
+++ app/src/main/java/com/example/ticker/MainActivity.kt
@@ -21,13 +21,17 @@ import javax.inject.Inject
 import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
+//ActivityMainBinding = activity_main.xml
 class MainActivity : AppCompatActivity() {
+
+
     private lateinit var binding: ActivityMainBinding
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //inflate(layoutInflater converts XML code to  Kotlin objects
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val navHostFragment= supportFragmentManager
--- app/src/main/java/com/example/ticker/ui/discover/DiscoverFragment.kt
+++ app/src/main/java/com/example/ticker/ui/discover/DiscoverFragment.kt
@@ -1,13 +1,11 @@
 package com.example.ticker.ui.discover
 
-import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.FragmentActivity
 import androidx.fragment.app.viewModels
 import com.example.ticker.viewmodel.DiscoverViewModel
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class DiscoverFragment: Fragment() {
-    private val ViewModel : DiscoverViewModel by viewModels()
+    private val viewModel : DiscoverViewModel by viewModels()
     }
