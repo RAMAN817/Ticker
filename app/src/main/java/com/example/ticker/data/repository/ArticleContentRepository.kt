d5fb8bcbcd153196649cc6ddd5ac8bf15441bd46
Rajan Bhusal <rajanbhusal@Rajans-MacBook-Pro-2.local>
2026-07-26 09:32:54 +1000

    added Readability in ArticleContentRepository

--- app/src/main/java/com/example/ticker/data/repository/ArticleContentRepository.kt
+++ app/src/main/java/com/example/ticker/data/repository/ArticleContentRepository.kt
@@ -3,8 +3,11 @@ package com.example.ticker.data.repository
 import com.example.ticker.data.model.ArticleContent
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import net.dankito.readability4j.Readability4J
 import okhttp3.OkHttpClient
 import javax.inject.Inject
+import okhttp3.Request
+import okio.IOException
 
 class ArticleContentRepository @Inject constructor(
     //Reusing okHttp not creating a new instance
@@ -13,7 +16,25 @@ private val okHttpClient: OkHttpClient
 {
     suspend fun extractArticle(url:String): Result<ArticleContent> =
         withContext(Dispatchers.IO){
-        try {
+            try {
+                val request = Request.Builder().url(url).build()
+                val html = okHttpClient.newCall(request).execute().use {
+                    response ->
+                     if (!response.isSuccessful) throw IOException("HTTP error ${response.code}")
+                    response.body?.string() ?: throw IOException("Empty response body ")
+
+                }
+                val readability = Readability4J(url , html)
+                val article = readability.parse()
+                val content = ArticleContent(
+                    title = article.title ?: "",
+                    textContent = article.textContent ?: "",
+                    imageUrl = article.imageUrl ?: ""
+
+
+                )
+            }
+            else{
 
         }
 
