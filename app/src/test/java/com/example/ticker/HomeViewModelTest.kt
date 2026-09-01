c94fb7da5341cf7e9fb2417f5dcce56a6dfabb24
Rajan Bhusal <rajanbhusal@Rajans-MBP-2.modem>
2026-09-01 11:05:58 +1000

    added testDispatcher

--- app/src/test/java/com/example/ticker/HomeViewModelTest.kt
+++ app/src/test/java/com/example/ticker/HomeViewModelTest.kt
@@ -1,6 +1,5 @@
 package com.example.ticker
 
-import com.example.ticker.data.api.NewsApiService
 import com.example.ticker.data.model.Article
 import com.example.ticker.data.repository.NewsRepository
 import com.example.ticker.viewmodel.HomeViewModel
@@ -12,10 +11,31 @@ import app.cash.turbine.*
 import com.example.ticker.ui.home.NewsUiState
 import junit.framework.TestCase.assertEquals
 import junit.framework.TestCase.assertTrue
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.test.StandardTestDispatcher
+import kotlinx.coroutines.test.setMain
+import kotlinx.coroutines.test.resetMain
+import org.junit.After
+import org.junit.Before
+
 
 class HomeViewModelTest {
+  //creating dispatcher for test to tell when and how to run the coroutines
+  private val testDispatcher = StandardTestDispatcher()
+
+  @Before
+  fun setup() {
+    Dispatchers.setMain(testDispatcher)
+  }
+
+  @After
+  fun tearDown() {
+    Dispatchers.resetMain()
+  }
+
   private val mock = mockk<NewsRepository>()
-@Test
+
+  @Test
   fun testViewModel() = runTest {
     coEvery {
       mock.getNews(any())
@@ -34,47 +54,47 @@ class HomeViewModelTest {
         )
       )
     )
-   val viewModel = HomeViewModel(mock)
-  //init{}  already fires loadNews, so general comes first
+    val viewModel = HomeViewModel(mock)
+    // init{} already fires loadNews, so general comes first
 
-  viewModel.newsState.test {
-    //awaitItem() -> Wait for the Flow to emit its next value, then give me that value
-    assertEquals(NewsUiState.Loading , awaitItem())
-    //initial's assigned with await because awaitItem() emit flow i.e changing states
-    val initial = awaitItem()
-    assertTrue(initial is NewsUiState.Success)
-    //Now the call that actually run this function
-    viewModel.loadNews("tech")
-    assertEquals(NewsUiState.Loading, awaitItem())
+    viewModel.newsState.test {
+      // awaitItem() -> Wait for the Flow to emit its next value, then give me that value
+      assertEquals(NewsUiState.Loading, awaitItem())
 
-    val result = awaitItem()
-    assertTrue(result is NewsUiState.Success)
-    val news = (result as NewsUiState.Success).news
-    assertEquals(1, news.size)
-    assertEquals("Trump planned xyz", news[0].headline)
+      testDispatcher.scheduler.advanceUntilIdle() // let the queued coroutine actually run now
 
-    cancelAndIgnoreRemainingEvents()
+      val initial = awaitItem()
+      assertTrue(initial is NewsUiState.Success)
 
+      // Now the call that actually run this function
+      viewModel.loadNews("tech")
+      testDispatcher.scheduler.advanceUntilIdle() // let THIS coroutine run to completion too
+
+      assertEquals(NewsUiState.Loading, awaitItem())
+
+      val result = awaitItem()
+      assertTrue(result is NewsUiState.Success)
+      val news = (result as NewsUiState.Success).news
+      assertEquals(1, news.size)
+      assertEquals("Trump planned xyz", news[0].headline)
+
+      cancelAndIgnoreRemainingEvents()
+    }
   }
 
-
-    }
   @Test
-  fun throwError() =  runTest{
+  fun throwError() = runTest {
     coEvery { mock.getNews(any()) } returns Result.failure(Exception("Error"))
     val viewModel = HomeViewModel(mock)
     viewModel.newsState.test {
       assertEquals(NewsUiState.Loading, awaitItem())
+
+      testDispatcher.scheduler.advanceUntilIdle() // let the queued coroutine actually run now
 
       val result = awaitItem()
       assertTrue(result is NewsUiState.Error)
       assertEquals("Error", (result as NewsUiState.Error).message)
       cancelAndIgnoreRemainingEvents()
     }
-
   }
-
-
-  }
-
-
+}
